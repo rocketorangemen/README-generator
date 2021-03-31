359eca24765b64117f448e4fbe7a78ce01c7943f
Mon <rocketorangemen@gmail.com>
2021-03-31 08:36:57 -0500

    license verification

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,22 @@
+let badge;
+let license;
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(projectData) {
+  if (projectData.license === "MIT") {
+    badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (projectData.license === "Apache-2.0") {
+    badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+  } else if (projectData.license === "Mozilla") {
+    badge = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
+  } else if (projectData.license === "ISC") {
+    badge = "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
+  } else {
+    badge = "No badge present";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
